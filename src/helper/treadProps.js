92d7005b290983d4ee37bd37c16ae03b1c08b465
jesse skralskis <jesseskralskis@gmail.com>
2020-11-23 15:24:56 -0500

    changed url for treadTrack

--- src/helper/treadProps.js
+++ src/helper/treadProps.js
@@ -24,7 +24,7 @@ const treadProps = {
     "App Store Connect",
     "Git",
   ],
-  seeCodeUrl: "https://github.com/JesseSkralskis/TreadTrackApi",
+  seeCodeUrl: "https://github.com/JesseSkralskis/TreadTrack",
   viewurl: "#",
   buttonColor: "#000000",
   headerColor: "#5f5f5f",
